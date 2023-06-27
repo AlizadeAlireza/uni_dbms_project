2dd95ad2a189af8231e62c7637824bfe721b3c8f
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-06-27 19:07:28 +0330

    adding basic abstracts

---
+++ a.py
@@ -0,0 +1,15 @@
+# this is the first commit for upload and create this repository
+
+# imports
+import pyodbc
+
+# connection details
+server = ""
+database = ""
+username = ""
+password = ""
+
+# create connection with f String --> f""
+
+
+# stablish connection
