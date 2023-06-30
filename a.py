f15bd7348162610cfc9add389d05df203be9526f
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-06-30 10:26:55 +0330

    connecting the library to SQL server

--- a.py
+++ a.py
@@ -4,12 +4,36 @@
 import pyodbc
 
 # connection details
-server = ""
-database = ""
-username = ""
+server = "MSI"
+database = "uni_project"
+username = "MSI\pc"
 password = ""
 
 # create connection with f String --> f""
-
+connection_string = f"Driver={{SQL Server}};Server={server};Database={database};Trusted_Connection=yes"
 
 # stablish connection
+
+# try: 
+#     connection = pyodbc.connect(connection_string)
+#     cursor = connection.cursor()
+
+#     # Execute a test query
+#     cursor.execute("SELECT 1")
+
+#     # Fetch the result
+#     result = cursor.fetchone()
+
+#     # Check if the result is as expected
+#     if result[0] == 1:
+#         print("Connection successful!")
+#     else:
+#         print("Connection failed!")
+
+#     # Close the cursor and connection
+#     cursor.close()
+#     connection.close()
+
+# except pyodbc.Error as e:
+#     print("Connection failed!")
+#     print(f"Error message: {str(e)}")
