7953cce391dff396c0e498ed8b91c6f1ebbdf97a
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-06-30 14:09:31 +0330

    connection function

--- a.py
+++ a.py
@@ -7,7 +7,7 @@ import pyodbc
 
 # connection function 
 def connection_to_db ():
-    
+
     # connection details
     server = "MSI"
     database = "uni_project"
@@ -16,6 +16,32 @@ def connection_to_db ():
 
     # create connection with f String --> f""
     connection_string = f"Driver={{SQL Server}};Server={server};Database={database};Trusted_Connection=yes"
+    #     stablish connection
+    # to ensure is connected!!
+
+    try: 
+        connection = pyodbc.connect(connection_string)
+        cursor = connection.cursor()
+
+        # Execute a test query
+        cursor.execute("SELECT 1")
+
+        # Fetch the result
+        result = cursor.fetchone()
+
+        # Check if the result is as expected
+        if result[0] == 1:
+            print("Connection successful!")
+        else:
+            print("Connection failed!")
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+    except pyodbc.Error as e:
+        print("Connection failed!")
+        print(f"Error message: {str(e)}")
 
 
 
@@ -23,32 +49,7 @@ def connection_to_db ():
 
 
 
+def main():
+    connection_to_db ()
 
-
-# stablish connection
-# to ensure is connected!!
-
-
-# try: 
-#     connection = pyodbc.connect(connection_string)
-#     cursor = connection.cursor()
-
-#     # Execute a test query
-#     cursor.execute("SELECT 1")
-
-#     # Fetch the result
-#     result = cursor.fetchone()
-
-#     # Check if the result is as expected
-#     if result[0] == 1:
-#         print("Connection successful!")
-#     else:
-#         print("Connection failed!")
-
-#     # Close the cursor and connection
-#     cursor.close()
-#     connection.close()
-
-# except pyodbc.Error as e:
-#     print("Connection failed!")
-#     print(f"Error message: {str(e)}")
+main()
