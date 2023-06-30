f0697368895d28987e4a26d5ac1cb3527c5b3b24
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-06-30 20:33:02 +0330

    main mnue abstration

--- menu.py
+++ menu.py
@@ -1,5 +1,17 @@
 '''
     in this script we want to create a menu class and use it in our logics.
     the different parts of our std_project for terminal user interface.
-    
+
 '''
+
+class Menu:
+    # main menu
+    def main_menu():
+        pass
+        # welcome masseage
+
+        # Register
+
+        # help about
+
+        # exit of the program
