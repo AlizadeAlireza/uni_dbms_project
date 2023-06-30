8331524ba670ec7dd94f65dcaa22edb030d1f77a
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-06-30 20:42:25 +0330

    main menu

--- menu.py
+++ menu.py
@@ -6,12 +6,16 @@
 
 class Menu:
     # main menu
-    def main_menu():
-        pass
+    def main_menu(self):
+        
         # welcome masseage
-
+        print("\n*** student project ***\n\n  please choose an option (-_-)\n")
         # Register
-
+        print("1, Register")
         # help about
-
+        print("2, help/about")
         # exit of the program
+        print("3, exit of the program")
+
+menu = Menu()
+main_menu = menu.main_menu()
