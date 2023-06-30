1476d56a5ddc06c8c21a30758e660a08ca15ebee
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-06-30 14:16:14 +0330

    decide to functional or oo

--- a.py
+++ a.py
@@ -3,51 +3,40 @@
 # imports
 import pyodbc
 
-
-
 # connection function 
 def connection_to_db ():
+    def test_connection():
+        # connection details
+        server = "MSI"
+        database = "uni_project"
+        # username = "MSI\pc"
+        # password = ""
 
-    # connection details
-    server = "MSI"
-    database = "uni_project"
-    username = "MSI\pc"
-    password = ""
+        # create connection with f String --> f""
+        connection_string = f"Driver={{SQL Server}};Server={server};Database={database};Trusted_Connection=yes"
+        #     stablish connection
+        # to ensure is connected!!
 
-    # create connection with f String --> f""
-    connection_string = f"Driver={{SQL Server}};Server={server};Database={database};Trusted_Connection=yes"
-    #     stablish connection
-    # to ensure is connected!!
+        try: 
+            connection = pyodbc.connect(connection_string)
+            cursor = connection.cursor()
+            # Execute a test query
+            cursor.execute("SELECT 1")
+            # Fetch the result
+            result = cursor.fetchone()
+            # Check if the result is as expected
+            if result[0] == 1:
+                print("Connection successful!")
+            else:
+                print("Connection failed!")
 
-    try: 
-        connection = pyodbc.connect(connection_string)
-        cursor = connection.cursor()
+            # Close the cursor and connection
+            cursor.close()
+            connection.close()
 
-        # Execute a test query
-        cursor.execute("SELECT 1")
-
-        # Fetch the result
-        result = cursor.fetchone()
-
-        # Check if the result is as expected
-        if result[0] == 1:
-            print("Connection successful!")
-        else:
+        except pyodbc.Error as e:
             print("Connection failed!")
-
-        # Close the cursor and connection
-        cursor.close()
-        connection.close()
-
-    except pyodbc.Error as e:
-        print("Connection failed!")
-        print(f"Error message: {str(e)}")
-
-
-
-
-
-
+            print(f"Error message: {str(e)}")
 
 def main():
     connection_to_db ()
