7795ab1ea1d6f6e28a63c3c995ec02895ebddb02
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-07-01 21:55:09 +0330

    change the query

--- tables.py
+++ tables.py
@@ -24,71 +24,71 @@ def create_tables():
 
     # SQL queries to create tables
     create_table_queries = [
-        """
-        CREATE TABLE STT (
-            STID CHAR(10) NOT NULL PRIMARY KEY,
-            STName VARCHAR(100),
-            STNJR char(20),
-            STDEID char(20)
-        )
-        """,
-        """
-        CREATE TABLE COT (
-            COID CHAR(20) NOT NULL PRIMARY KEY,
-            COTitle VARCHAR(100),
-            Credit INT,
-            COtype CHAR(20)
-        )
-        """,
-        """
-        CREATE TABLE STCOT (
-            STID CHAR(10),
-            COID CHAR(10),
-            TR INT,
-            YRYR INT,
-            Grade Float,
-            PRIMARY KEY (TR, YRYR),
-            FOREIGN KEY(STID) REFERENCES STT(STID),
-            FOREIGN KEY(COID) REFERENCES COT(COID)
-        )
-        """,
-        """
-        CREATE TABLE PREREQ (
-            COID CHAR(10),
-            PRECOID CHAR(10) PRIMARY KEY,
-            FOREIGN KEY(COID) REFERENCES COT(COID)
-        )
-        """,
-        """
-        CREATE TABLE DEPT (
-            MGRID CHAR(10),
-            DEID CHAR(10) NOT NULL PRIMARY KEY,
-            DEtitle varchar(100),
-            phone char(12),
-            address varchar(200),
-            FOREIGN KEY(MGRID) REFERENCES PROF(PRID)
-        )
-        """,
-        """
-        CREATE TABLE PROF (
-            PRID char(10) NOT NULL PRIMARY KEY,
-            PRname varchar(100),
-            RANKK char(20),
-            DEID CHAR(10),
-            FOREIGN KEY(DEID) REFERENCES DEPT(DEID)
-        )
-        """,
-        """
-        CREATE TABLE PRCO (
-            PRID char(10),
-            COID char(10),
-            GROUP_ID char(10),
-            PRIMARY KEY (PRID, COID, GROUP_ID),
-            FOREIGN KEY(PRID) REFERENCES PROF(PRID),
-            FOREIGN KEY(COID) REFERENCES COT(COID)
-        )
-        """
-    ]
+    """
+    CREATE TABLE STT (
+        STID CHAR(10) NOT NULL PRIMARY KEY,
+        STName VARCHAR(100),
+        STNJR CHAR(20),
+        STDEID CHAR(20)
+    )
+    """,
+    """
+    CREATE TABLE COT (
+        COID CHAR(10) NOT NULL PRIMARY KEY,
+        COTitle VARCHAR(100),
+        Credit INT,
+        COtype CHAR(20)
+    )
+    """,
+    """
+    CREATE TABLE STCOT (
+        STID CHAR(10),
+        COID CHAR(10),
+        TR INT,
+        YRYR INT,
+        Grade FLOAT,
+        PRIMARY KEY (STID, COID, TR, YRYR),
+        FOREIGN KEY (STID) REFERENCES STT(STID),
+        FOREIGN KEY (COID) REFERENCES COT(COID)
+    )
+    """,
+    """
+    CREATE TABLE PREREQ (
+        COID CHAR(10),
+        PRECOID CHAR(10) PRIMARY KEY,
+        FOREIGN KEY (COID) REFERENCES COT(COID)
+    )
+    """,
+    """
+    CREATE TABLE DEPT (
+        MGRID CHAR(10),
+        DEID CHAR(10) NOT NULL PRIMARY KEY,
+        DEtitle VARCHAR(100),
+        phone CHAR(12),
+        address VARCHAR(200),
+        FOREIGN KEY (MGRID) REFERENCES PROF(PRID)
+    )
+    """,
+    """
+    CREATE TABLE PROF (
+        PRID CHAR(10) NOT NULL PRIMARY KEY,
+        PRname VARCHAR(100),
+        RANKK CHAR(20),
+        DEID CHAR(10),
+        FOREIGN KEY (DEID) REFERENCES DEPT(DEID)
+    )
+    """,
+    """
+    CREATE TABLE PRCO (
+        PRID CHAR(10),
+        COID CHAR(10),
+        GROUP_ID CHAR(10),
+        PRIMARY KEY (PRID, COID, GROUP_ID),
+        FOREIGN KEY (PRID) REFERENCES PROF(PRID),
+        FOREIGN KEY (COID) REFERENCES COT(COID)
+    )
+    """
+]
 
     try:
         # Execute the queries to create the tables
