23e42ff66281d66370a3b11b2211c9c165367c59
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-07-03 00:14:57 +0330

    put hints on all the abilities function

--- abilities.py
+++ abilities.py
@@ -6,23 +6,37 @@ class Abilitiy:
     # a = "for student"
     def available_courses(self):
         pass
+        # it is about fetching all the cources and print it
     def show_informations(self):
         pass
+        # maybe i delete this
+        # maybe can fetch the row and print the informations
     def select_course(self):
         pass
+        # in the available courses
+        # user can select the course and submit it in stcot table
     def remove_course(self):
         pass
+        # user can delete the course that select 
     def total_average(self):
         pass
+        # it is a query and after execute we can return it
+        # the query get all the grades for that student
     def term_average(self):
         pass
+        # it is a query and after execute we can return it
+        # the query get just the term grades for that student
 
 class AdminAbility(Abilitiy):
     id = 0
     def remove_default_course(self):
         pass
+        # can drop the row with first fetching and after delete that row
     def add_course(self):
         pass
+        # like add student
+        # we pass the parameters in the query 
+        # just like add students
     def add_student(self):
         database_connection = Connection()
 
@@ -42,6 +56,8 @@ class AdminAbility(Abilitiy):
         
     def add_professor(self):
         pass
+        # like add student we 
+        # pass the elemnts and execute the query
 
 class StudentAbility(Abilitiy):
     pass
