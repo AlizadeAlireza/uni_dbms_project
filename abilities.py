b9ad68274d0198589927334fb549b2a1c8e7dd11
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-07-03 09:21:29 +0330

    change tables to auto inc

--- abilities.py
+++ abilities.py
@@ -17,7 +17,7 @@ class Abilitiy:
         # user can select the course and submit it in stcot table
     def remove_course(self):
         pass
-        # user can delete the course that select 
+        # user can delete the course that select ---> delete from STCOT
     def total_average(self):
         pass
         # it is a query and after execute we can return it
@@ -40,19 +40,19 @@ class AdminAbility(Abilitiy):
     def add_student(self):
         database_connection = Connection()
 
-        id = str(self.id)
+        # id = str(self.id)
         name = input(" name : ")
         major = input (" major ")
         level = input (" bs / phd / ms ") # hint : helper function
         de_id = department_id()
         query = "INSERT INTO STT (STID, STName, STMJR, STLev, STDEID) VALUES (?,?,?,?,?)"
-        values = (id, name, major, level, de_id)
+        values = (name, major, level, de_id)
 
         cursor, connection = database_connection._open()
         database_connection._execute(cursor, connection, query, values)
         database_connection._close(cursor,connection)
 
-        self.id += 1   #error id is not correct !!
+        # self.id += 1   #error id is not correct !!
         
     def add_professor(self):
         pass
--- tables.py
+++ tables.py
@@ -24,73 +24,67 @@ def create_tables():
 
     # SQL queries to create tables
     create_table_queries = [
-    """
-    CREATE TABLE STT (
-        STID CHAR(20) NOT NULL PRIMARY KEY,
+    """CREATE TABLE STT (
+        STID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
         STName CHAR(100),
         STMJR CHAR(20),
         STLev CHAR(20),
         STDEID CHAR(20)
-    )
-    """,
-    """
-   CREATE TABLE COT (
-        COID CHAR(20) NOT NULL PRIMARY KEY,
-		CODEID CHAR(20),
-        COTitle VARCHAR(100),
-        Credit INT,
-        COtype CHAR(20)
-    )
-    """,
-    """
-    CREATE TABLE STCOT (
-        STID CHAR(20),
-        COID CHAR(20),
-        TR INT,
-        YRYR INT,
-        Grade FLOAT,
-        PRIMARY KEY (STID, COID, TR, YRYR),
-        FOREIGN KEY (STID) REFERENCES STT(STID),
-        FOREIGN KEY (COID) REFERENCES COT(COID)
-    )
-    """,
-    """
-    CREATE TABLE PREREQ (
-        COID CHAR(20),
-        PRECOID CHAR(20),
-		PRIMARY KEY (COID, PRECOID),
-        FOREIGN KEY (COID) REFERENCES COT(COID)
-    )
-    """,
-    """
-    CREATE TABLE DEPT(
-	DEID CHAR(20) NOT NULL PRIMARY KEY,
-	MGRID CHAR(20),
-    DEtitle varchar(100),
-    phone char(12),
-    address varchar(200),
-    )
-    """
-      ,
-    """
-    CREATE TABLE PROF (
-        PRID CHAR(20) NOT NULL PRIMARY KEY,
-        PRname VARCHAR(100),
-        RANKK CHAR(20),
-        DEID CHAR(20),
-        FOREIGN KEY (DEID) REFERENCES DEPT(DEID)
-    )
-    """,
-    """
-    CREATE TABLE PRCO (
-        PRID CHAR(20),
-        COID CHAR(20),
-        GROUP_ID CHAR(10),
+    )""",
+
+    """CREATE TABLE COT (
+            COID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+            CODEID CHAR(20),
+            COTitle VARCHAR(100),
+            Credit INT,
+            COtype CHAR(20)
+    )"""
+    ,
+
+    """CREATE TABLE STCOT (
+            STID INT,
+            COID INT,
+            TR INT,
+            YRYR INT,
+            Grade FLOAT,
+            PRIMARY KEY (STID, COID, TR, YRYR),
+            FOREIGN KEY (STID) REFERENCES STT(STID),
+            FOREIGN KEY (COID) REFERENCES COT(COID)
+    )""",
+
+    """CREATE TABLE PREREQ (
+            COID INT,
+            PRECOID CHAR(20),
+            PRIMARY KEY (COID, PRECOID),
+            FOREIGN KEY (COID) REFERENCES COT(COID)
+    )"""
+    ,
+    """CREATE TABLE DEPT(
+        DEID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        MGRID CHAR(20),
+        DEtitle varchar(100),
+        phone char(12),
+        address varchar(200),
+    )"""
+    ,
+    """CREATE TABLE PROF (
+            PRID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+            PRname VARCHAR(100),
+            RANKK CHAR(20),
+            DEID INT,
+            FOREIGN KEY (DEID) REFERENCES DEPT(DEID)
+    )""",
+    """CREATE TABLE PRCO(
+        PRID INT,
+        COID INT,
+        GROUP_ID INT IDENTITY(1,1) NOT NULL,
         PRIMARY KEY (PRID, COID, GROUP_ID),
-        FOREIGN KEY (PRID) REFERENCES PROF(PRID),
-        FOREIGN KEY (COID) REFERENCES COT(COID)
-    )
-    """
+        foreign key (PRID) references PROF(PRID),
+        foreign key (COID) references COT(COID)
+    )"""
+
+
+
     
 ]
 
