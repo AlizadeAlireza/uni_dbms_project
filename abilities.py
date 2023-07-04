8a57faed180c67e6ce45f46d122b525e5d685a51
AlizadeAlireza <alireza.aa9700@gmail.com>
2023-07-04 06:30:34 +0330

    finish the 99% of error handling

--- abilities.py
+++ abilities.py
@@ -4,60 +4,79 @@ from connections import Connection
 class Abilitiy:
     def select_course(self, student_id):     #only admin can add the grade 
         database_connection = Connection()
+        try:
+            lesson_id = int(input("enter lesson ID: "))
+            term = int(input("term : "))
+            year = input(" year : ")
+            grade = input (" grade:  ")
+            
+            query = "INSERT INTO STCOT (STID, COID, TR, YRYR, Grade) VALUES (?,?,?,?,?)"
+            values = (student_id,lesson_id, term, year, grade)
 
-        lesson_id = int(input("enter lesson ID: "))
-        term = int(input("term : "))
-        year = input(" year : ")
-        grade = input (" grade:  ")
-        
-        query = "INSERT INTO STCOT (STID, COID, TR, YRYR, Grade) VALUES (?,?,?,?,?)"
-        values = (student_id,lesson_id, term, year, grade)
-
-        cursor, connection = database_connection._open()
-        database_connection._execute(cursor, connection, query, values)
-        database_connection._close(cursor,connection)
+            cursor, connection = database_connection._open()
+            database_connection._execute(cursor, connection, query, values)
+            database_connection._close(cursor,connection)
+            print("\nselect the course successfully!")
+        except Exception as e:
+            print("An error occurred while adding the course:")
+            print(e)
         # in the available courses
         # user can select the course and submit it in stcot table
     def remove_course(self, student_id):
         database_connection = Connection()
-        lesson_id = int(input("enter lesson ID: "))
-        query = "DELETE from STCOT where STCOT.STID = ? AND STCOT.COID = ?"
-        values = (student_id,lesson_id)
+        try:
+            lesson_id = int(input("enter lesson ID: "))
+            query = "DELETE from STCOT where STCOT.STID = ? AND STCOT.COID = ?"
+            values = (student_id,lesson_id)
 
-        cursor, connection = database_connection._open()
-        database_connection._execute(cursor, connection, query, values)
-        database_connection._close(cursor,connection)
+            cursor, connection = database_connection._open()
+            database_connection._execute(cursor, connection, query, values)
+            database_connection._close(cursor,connection)
 
+            print("\nremove the course successfully!!")
+        except Exception as e:
+            print("An error occurred while adding the course:")
+            print(e)
         # user can delete the course that select ---> delete from STCOT
     
         
     def student_average(self, student_id):
         database_connection = Connection()
-        query = f""" select STT.STNAME,STT.STID,
-            sum(STCOT.Grade*COT.CREDIT)/sum(COT.CREDIT) as AVE from
-            STT,COT,STCOT
-            where STT.STID = STCOT.STID  and COT.COID = STCOT.COID and STT.STID = {student_id}
-            group by STT.STNAME,STT.STID """
+        try:
+            query = f""" select STT.STNAME,STT.STID,
+                sum(STCOT.Grade*COT.CREDIT)/sum(COT.CREDIT) as AVE from
+                STT,COT,STCOT
+                where STT.STID = STCOT.STID  and COT.COID = STCOT.COID and STT.STID = {student_id}
+                group by STT.STNAME,STT.STID """
 
 
-        cursor, connection = database_connection._open()
-        database_connection._execute(cursor, connection, query, "")
-        database_connection._fetch(cursor)
-        database_connection._close(cursor,connection)
+            cursor, connection = database_connection._open()
+            database_connection._execute(cursor, connection, query, "")
+            database_connection._fetch(cursor)
+            database_connection._close(cursor,connection)
+        except Exception as e:
+            print("An error occurred while calculating the student average!")
+            print(e)
 
 class AdminAbility(Abilitiy):
     def remove_default_course(self):
         database_connection = Connection()
 
-         # hint : helper function
-        lesson_id = int(input("\nenter lesson ID : "))
-        delete_query = "DELETE FROM COT WHERE COID = ?"
-        values = (lesson_id)
+        # hint : helper function
+        try:
+            lesson_id = int(input("\nenter lesson ID : "))
+            delete_query = "DELETE FROM COT WHERE COID = ?"
+            values = (lesson_id)
 
-        cursor, connection = database_connection._open()
-        database_connection._execute(cursor, connection, delete_query, values)
-        database_connection._close(cursor,connection)
-        # can drop the row with first fetching and after delete that row
+            cursor, connection = database_connection._open()
+            database_connection._execute(cursor, connection, delete_query, values)
+            database_connection._close(cursor,connection)
+            # can drop the row with first fetching and after delete that row
+
+            print("\n remove the default course by admin successfully!")
+        except Exception as e:
+            print("An error occurred while adding the course:")
+            print(e)
     def add_course(self):
         database_connection = Connection()
 
@@ -77,23 +96,30 @@ class AdminAbility(Abilitiy):
             print("\nCourse added successfully!")
 
         except Exception as e:
-            print("An error occurred while adding the course:")
+            print("An error occurred while adding the course!")
             print(e)
 
 
     def add_student(self):
         database_connection = Connection()
 
-        name = input("name : ")
-        major = input("major: ")
-        level = student_level() # hint : helper function
-        de_id = department_id()
-        query = "INSERT INTO STT (STName, STMJR, STLev, STDEID) VALUES (?,?,?,?)"
-        values = (name, major, level, de_id)
+        try:
+            name = input("name : ")
+            major = input("major: ")
+            level = student_level() # hint : helper function
+            de_id = department_id()
+            query = "INSERT INTO STT (STName, STMJR, STLev, STDEID) VALUES (?,?,?,?)"
+            values = (name, major, level, de_id)
 
-        cursor, connection = database_connection._open()
-        database_connection._execute(cursor, connection, query, values)
-        database_connection._close(cursor,connection)
+            cursor, connection = database_connection._open()
+            database_connection._execute(cursor, connection, query, values)
+            database_connection._close(cursor,connection)
+
+            print("\nStudent added successfully!")
+
+        except Exception as e:
+            print("An error occurred while adding the student!")
+            print(e)
         
     def add_professor(self):
         pass
@@ -102,18 +128,21 @@ class AdminAbility(Abilitiy):
 
     def total_average(self):
         database_connection = Connection()
-        query = f""" select STT.STNAME,STT.STID,
+        try:
+            query = f""" select STT.STNAME,STT.STID,
             sum(STCOT.Grade*COT.CREDIT)/sum(COT.CREDIT) as AVE from
             STT,COT,STCOT
             where STT.STID = STCOT.STID  and COT.COID = STCOT.COID
             group by STT.STNAME,STT.STID """
 
 
-        cursor, connection = database_connection._open()
-        database_connection._execute(cursor, connection, query, "")
-        database_connection._fetch(cursor)
-        database_connection._close(cursor,connection)
-
+            cursor, connection = database_connection._open()
+            database_connection._execute(cursor, connection, query, "")
+            database_connection._fetch(cursor)
+            database_connection._close(cursor,connection)
+        except Exception as e:
+            print("An error occurred while calculating the average.")
+            print(e)
     # def remove_student(self, student_id):
     #     database_connection = Connection()
 
@@ -127,6 +156,3 @@ class AdminAbility(Abilitiy):
 
 class StudentAbility(Abilitiy):
     pass
-
-# b = AdminAbility()
-# print(b.remove_student(1))
--- helper_script.py
+++ helper_script.py
@@ -2,7 +2,7 @@ admin_username = "admin"
 admin_password = "admin"
 
 def department_id():
-    department = input("1, fanni /2, ensani/3, tarbiat" )
+    department = input("1, fanni /2, ensani/3, tarbiat: " )
     try:
         if department == "1":
             stdeid = "1"
@@ -12,7 +12,7 @@ def department_id():
             stdeid = "3"
         return stdeid
     except:
-        return "there is no such DEPARTMENT!"
+        print("\nthere is no such DEPARTMENT!")
     
 def lesson_type():
     type = input("\n1, amali /2, nazari: " )
@@ -21,9 +21,13 @@ def lesson_type():
             lesson_type = "amali"
         elif type == "2":
             lesson_type = "nazari"
+        else:
+            print("\nthere is no such lesson type!")
+
         return lesson_type
+        
     except:
-        return "there is no such DEPARTMENT!"
+        print("\nthere is no such lesson type!")
 
 def help():
     a = "\nthis is a test for our help function to show that." 
@@ -35,7 +39,7 @@ def about():
 
 
 def student_level():
-    level = input(" \nselect the level: 1, bs / 2, phd / 3, ms")
+    level = input(" \nselect the level: 1, bs / 2, phd / 3, ms: ")
     try:
         if level == "1":
             stu_level = "bs"
